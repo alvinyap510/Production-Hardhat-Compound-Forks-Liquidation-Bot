3b654213d1f9c3e2883c809e6765fa9737fd3ab6
Alvin Yap <leaf.alvin@gmail.com>
2023-11-04 17:03:00 +0800

    chore: refactor deployer

--- scripts/deployer.js
+++ scripts/deployer.js
@@ -14,10 +14,18 @@ const signer = ethers.provider.getSigner();
 let tx;
 
 /********************/
-/* Recreate Instances */
+/* Configuration */
 /********************/
+/* Aave or Aave Forks Protocol */
+const LENDING_POOL_ADDRESSES_PROVIDER =
+  "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5";
+/* Uniswap V2 or Forks Factory */
+const UNISWAP_V2_FACTORY = "0x1111111254fb6c44bAC0beD2854e76F90643097d";
+/* Uniswap V2 or Forks Router */
+const UNISWAP_V2_ROUTER = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+/* Blockchain Wrapped Token Address */
+const WETH = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 
-//@Author
 /********************/
 /* Main Body */
 /********************/
@@ -28,10 +36,10 @@ async function main() {
   );
   const CompoundForksLiquidationBotV2 =
     await CompoundForksLiquidationBotV2Factory.deploy(
-      "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
-      "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-      "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
-      "0x1111111254fb6c44bAC0beD2854e76F90643097d",
+      LENDING_POOL_ADDRESSES_PROVIDER,
+      UNISWAP_V2_ROUTER,
+      WETH,
+      UNISWAP_V2_FACTORY,
       { gasLimit: 30000000 }
     );
   await CompoundForksLiquidationBotV2.deployed();
